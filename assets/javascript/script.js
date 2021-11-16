8578643c304260b3b0e871e97afbb2d5e7ea8949
samhodgson88 <sam.hodgson.coding@gmail.com>
2021-11-16 22:56:34 +0000

    updated CSS

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,51 @@
-console.log("connected!")
+
+// wait for DOM to finish loading before running the game
+// Get the button elements and add event listeners (button clicks etc) to them
+// let button of buttons is modern version of let i = 0; i < buttons.length; i++)
+document.addEventListener("DOMcontentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+     
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit") {
+                alert("you clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+
+        })
+    }
+})
+function runGame(){
+
+}
+
+
+
+
+function checkAnswer(){
+
+}
+function calculatecorrectAnswer {
+
+}
+function IncrementScore(){
+
+}
+function IncrementWrongAnswer(){
+
+}
+function DisplayAdditionQuestion(){
+
+}
+function DisplaySubtractQuestion(){
+
+}
+function DisplaymultiplicationQuestion(){
+
+}
+function DisplayDivisionQuestion(){
+
+}
